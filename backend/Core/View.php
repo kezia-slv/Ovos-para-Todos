98f9d5ae59a43795ff1cded2aed0cfab2038de2c
kezia.slv <keziaoficialk@gmail.com>
2026-02-17 23:54:02 -0300

    Arquivos de configuração do BD adicionados!!

---
+++ backend/Core/View.php
@@ -0,0 +1,34 @@
+<?php
+namespace Ovos\Ebenezer\Core;
+class View
+{
+    /**
+     * Renderiza uma view.
+     * @param string $nomeView Nome da view (ex: 'home/index')
+     * @param array $dados Dados a serem passados para a view.
+     */
+    public static function render(string $nomeView, array $dados = [])
+    {
+        // Proteção contra LFI: Sanitiza o nome da view removendo tentativas de traversal
+        $nomeView = str_replace(['../', '..\\'], '', $nomeView);
+        $caminhoView = __DIR__ . "/../Views/templates/{$nomeView}.php";
+
+        if (!file_exists($caminhoView)) {
+            throw new \Exception("A view '{$nomeView}' não foi encontrada.");
+        }
+
+        extract($dados);
+
+        // Verifica tipo de usuário para carregar o header correto
+        if (isset($_SESSION['usuario_tipo']) && $_SESSION['usuario_tipo'] === 'Cliente') {
+            require_once __DIR__ . "/../Views/templates/admin/cliente/partials/header.php";
+        } else {
+            require_once __DIR__ . "/../Views/templates/partials/header.php";
+        }
+
+        require_once $caminhoView;
+
+        require_once __DIR__ . "/../Views/templates/partials/footer.php";
+    }
+
+}
